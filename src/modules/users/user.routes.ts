a60ce74dd6b591942fa090e3f1db6027e360cac6
Daniel Wattimury <daniel032902@gmail.com>
2026-08-16 03:06:05 +0800

    feat(users): wire createUserController into users routes

--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { useRoute } from '../../common/http/useRoute.ts';
-import { getAllUsersController } from './user.controller.ts';
+import { getAllUsersController, createUserController } from './user.controller.ts';
 import type { UseUsersRoute } from './user.dto.ts';
 
 const routes = Router();
 routes.use(getAllUsersController);
+routes.use(createUserController);
 
 const usersRouter = Router();
 usersRouter.use(...useRoute<[UseUsersRoute]>('/users', routes));
